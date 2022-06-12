ae50a1944eb234e0d52c4e40dc9b548ecfcfd0f4
surbhi <surbhisaga337865@gmail.com>
2022-06-12 18:33:03 +0530

    auth

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/usersSlice";
-import authSlice from "./users/AuthSlice";
+import authReducer from "./users/AuthSlice";
 
 export default configureStore({
-  reducer: { users: usersReducer, authuser: authSlice },
+  reducer: { users: usersReducer, authuser: authReducer },
 });
